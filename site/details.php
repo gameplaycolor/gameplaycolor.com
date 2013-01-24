6898d06d1cab224dc3f39a7d4e409e8298201add
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-24 08:27:37 +0000

    Minor layout tweaks to the copyright notice.

--- site/details.php
+++ site/details.php
@@ -36,8 +36,10 @@
 
 
   <div class="footer">
-    <p>Copyright &copy; InSeven Limited 2013. All rights reserved.</p>
-    <p>InSeven Limited, Registered in England and Wales, Number 7082463.</p>    
+    <p>
+      Copyright &copy; InSeven Limited 2013. All rights reserved.<br/>
+      InSeven Limited, Registered in England and Wales, Number 7082463.
+    </p>
   </div>
 
 </div> <!-- /container -->
