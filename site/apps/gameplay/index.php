ba22edee0c18033efdeff2b41012b95b2a2404b5
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-25 20:54:47 +0000

    Adding a button for installing the game boy emulator.

--- site/apps/gameplay/index.php
+++ site/apps/gameplay/index.php
@@ -15,7 +15,7 @@
       <img src="images/phones.png" />
       <!-- <h1>Game Play</h1> -->
       <p class="lead tagline">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-
+      <a href="#myModal" role="button" class="btn btn-primary" data-toggle="modal">Install Now</a>
     </div>
   </div>
 </div>
