3b507635315ee6fec13fd2901b79193ff8da585a
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-26 11:39:18 +0000

    Adding a custom 404.

---
+++ site/404.php
@@ -0,0 +1,28 @@
+<?php
+
+  include 'inseven.php';
+
+  head("Not Found");
+  navigation("404");
+
+?>
+
+<div class="full-width sketchpad">
+  <div class="container-narrow">
+  
+    <div class="jumbotron">
+      <h1 class="logo">Not Found.</h1>
+    </div>
+    <div class="jumbotron">
+      <p class="lead tagline bright">The page you are looking for cannot be found.</p>
+    </div>
+
+  </div>
+</div>
+
+<?php
+
+  details();
+  footer();
+
+?>
