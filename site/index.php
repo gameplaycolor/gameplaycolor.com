36e1a7f8cdd7ba7e412d53e0f33b4ef043e38fec
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-27 11:57:03 +0000

    Improvements to the front page.

--- site/index.php
+++ site/index.php
@@ -7,16 +7,52 @@
 
 ?>
 
-<div class="full-width sketchpad">
+<div class="full-width texture">
   <div class="container-narrow">
   
     <div class="jumbotron">
       <h1 class="logo">InSeven Limited.</h1>
     </div>
     <div class="jumbotron">
-      <p class="lead tagline bright">InSeven Limited build great apps like <a href="/apps/gameplay/">Game Play</a>, and offer bespoke development and consultancy.  We specialise in mobile software, with a particular focus on iOS and Android.</p>
+      <p class="lead tagline bright">InSeven build great apps and offer bespoke development and consultancy.<br />  We specialise in mobile software, with a particular focus on iOS and Android.</p>
     </div>
 
+  </div>
+</div>
+
+<div class="container-narrow">
+
+  <div class="features">
+
+    <div class="row-fluid">
+      <div class="span4">
+      </div>
+      <div class="span8">
+        <div class="feature">
+          <h4><i class="icon-hdd"></i> Google Drive</h4>
+          <p>Store all your Game Boy ROMs and cover art in Google Drive.  Adding a game to Game Play is as simple as dragging-and-dropping it into your Google Drive and opening the game library.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row-fluid">
+      <div class="span8">
+        <div class="feature">
+          <h4><i class="icon-eye-open"></i> Great Apps</h4>
+          <p>Game Play automatically identifies cover art stored alongside your ROMs and shows it on beautifully rendered cartridges. Simply drop a JPEG or PNG file with the same name as your game into Google Drive and Game Play does the rest.</p>
+        </div>
+      </div>
+      <div class="span4">
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+<div class="full-width sketchpad">
+  <div class="container-narrow">
+    <div class="row-fluid marketing">
+    </div>
   </div>
 </div>
 
