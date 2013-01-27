a0ff01ca9e9639af6fb0471bf180881385646bf9
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-01-27 10:30:44 +0000

    Removing the learn assets.

--- site/apps.php
+++ site/apps.php
@@ -11,16 +11,10 @@
   <div class="container-narrow">
 
     <div class="row-fluid features icons">
-      <div class="span6">
+      <div class="span12">
         <div class="icon lead tagline">
           <a href="/apps/gameplay/"><img src="/images/ico_gameplay.png" /></a><br/>
           <a href="/apps/gameplay/">Game Play</a> &mdash; Game Boy emulator for the iPhone 5
-        </div>
-      </div>
-      <div class="span6">
-        <div class="icon lead tagline">
-          <a href="/apps/learn/"><img src="/images/ico_learn.png" /></a><br/>
-          <a href="/apps/learn/">Learn</a> &mdash; Flash cards that never let you forget to study
         </div>
       </div>
     </div>
