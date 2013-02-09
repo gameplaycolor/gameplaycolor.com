fc77e8f493fbc5e269b24279e4165cce35c649df
Jason Barrie Morley <jbmorley@inseven.co.uk>
2013-02-09 11:34:15 +0000

    Fixing a link and adding an FAQ.

--- site/apps_gameplay.php
+++ site/apps_gameplay.php
@@ -46,11 +46,38 @@
       <div class="span6">
         <div class="feature">
           <h4><i class="icon-github"></i> Open Source</h4>
-          <p>Licensed under the GPL and available on <a href="https://github.com/jbmorley/game-play">Github</a>, Game Play is open source through-and-through.  Game Boy emulation uses the <a href="http://www.codebase.es/jsgb/">JSGB</a> emulator core.</p>
+          <p>Licensed under the GPL and available on <a href="https://github.com/jbmorley/gameplay">Github</a>, Game Play is open source through-and-through.  Game Boy emulation uses the <a href="http://www.codebase.es/jsgb/">JSGB</a> emulator core.</p>
         </div>
      </div>
     </div>
+    <hr />
+  </div>
 
+  <div class="container-narrow">
+    <div class="features">
+        <div class="row-fluid">
+        <div class="span12">
+          <div class="feature">
+            <h4 id="faq"><i class="icon-question-sign"></i> FAQ</h4>
+            <ol>
+              <li>
+                <h5>Why does Game Play not work on my iPhone 4/4S?</h5>
+                <p>Game Play performance is currently quite poor on the iPhone 4 and iPhone 4S, so we've restricted it to the iPhone 5 for initial release.</p>
+              </li>
+              <li>
+                <h5>Why am I getting an 'Unknown memory bank controller' error?</h5>
+                <p>Game Play does not currently support Game Boy Color ROMs.  Support will be coming in future releases.</p>
+              </li>
+              <li>
+                <h5>Why can't I play my Pokemon ROMs?</h5>
+                <p>See the previous answer.</p>
+              </li>
+            </ol>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   
 </div>
