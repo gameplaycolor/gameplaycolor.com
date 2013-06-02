b3acbe89cda3f368c3c805fe065449923c8d5c47
Jason Morley <jbmorley@inseven.co.uk>
2013-06-02 12:40:48 +0000

    Adding a list of supported games to the FAQ.

--- site/apps_gameplay.php
+++ site/apps_gameplay.php
@@ -61,6 +61,9 @@
             <h4 id="faq"><i class="icon-question-sign"></i> FAQ</h4>
             <ol>
               <li>
+                <h5>Which games work with Game Play?<h5>
+                <p>Game Play supports a wide range of Game Boy original ROMs.  Games that are known to work include: <strong>Adventure Island</strong>, <strong>Adventure Island 2</strong>, <strong>Bionic Commando</strong>, <strong>Bram Stoker's Dracula</strong>, <strong>Castle Quest</strong>, <strong>Castlevania 2 - Belmont's Revenge</strong>, <strong>Dr. Mario</strong>, <strong>Duck Tales 1</strong>, <strong>Duck Tales 2</strong>, <strong>Fist of the North Star</strong>, <strong>Kirby's Dream Land</strong>, <strong>Mega Man 1</strong>, <strong>Mega Man 2</strong>, <strong>Mega Man 3</strong>, <strong>Mystical Ninja</strong>, <strong>Ninja Boy 1</strong>, <strong>Ninja Boy 2</strong>, <strong>Ninja Gaiden Shadow</strong>, <strong>Othello</strong>, <strong>Ren &amp; Stimpy Space Cadet Advenutres</strong>, <strong>Super Mario World</strong>, <strong>Tale Spin</strong>, <strong>Tecmo Bowl</strong>, <strong>The Castlevania Adventure ('86)</strong>, <strong>The Flash</strong>, <strong>The Lion King</strong>, <strong>The Little Mermaid</strong>, <strong>The Pagemaster</strong>, <strong>The Real Ghostbusters</strong>, <strong>Tiny Toon Adventures - Montana's Movie Madness</strong>, <strong>Tiny Toon Advenutres - Bab's Big Break</strong>, <strong>TMNT - Fall of the Foot Clan</strong>, <strong>TMNT II - Back from the Swewers</strong>, <strong>TMNT III - Radical Rescue</strong>, <strong>Yoshi's Cookie</strong>.</p>
+              <li>
                 <h5>Why does Game Play not work on my iPhone 4/4S?</h5>
                 <p>The iPhone 4 and iPhone 4S are not fast enough to run Game Play.</p>
               </li>
