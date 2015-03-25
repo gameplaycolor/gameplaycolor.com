cb9e778935d238930cd1e5a54d295565bedad1f5
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-03-25 11:06:31 +0100

    Google Drive is more important.

--- site/apps_gameplaycolor.php
+++ site/apps_gameplaycolor.php
@@ -38,17 +38,17 @@ navigation("/apps/gameplaycolor/");
     <div class="row-fluid">
       <div class="span6">
         <div class="feature">
-          <i class="feature-icon fa fa-image fx-fw"></i>
-          <h4>Beautiful Cover Art</h4>
-          <p>Game Play Color automatically identifies cover art stored alongside your ROMs and shows it on beautifully rendered cartridges.</p>
+          <i class="feature-icon fa fa-google fa-fw"></i>
+          <h4>Google Drive</h4>
+          <p>Store all your Game Boy and Game Boy Color ROMs<sup>2</sup> and cover art in Google Drive.</p>
+          <p class="detail">Simply dragging-and-drop your games into Google Drive.</p>
         </div>
       </div>
       <div class="span6">
         <div class="feature">
-	  <i class="feature-icon fa fa-google fa-fw"></i>
-          <h4>Google Drive</h4>
-          <p>Store all your Game Boy and Game Boy Color ROMs<sup>2</sup> and cover art in Google Drive.</p>
-          <p class="detail">Simply dragging-and-drop your games into Google Drive.</p>
+          <i class="feature-icon fa fa-image fx-fw"></i>
+          <h4>Beautiful Cover Art</h4>
+          <p>Game Play Color automatically identifies cover art stored alongside your ROMs and shows it on beautifully rendered cartridges.</p>
         </div>
       </div>
     </div>
