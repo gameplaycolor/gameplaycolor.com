6dea84094232041a4db0a37492ea35adcd19f8f4
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-04-29 22:36:59 +0200

    Adding top links.

--- gameplaycolor-microsite/faq.php
+++ gameplaycolor-microsite/faq.php
@@ -34,7 +34,9 @@ navigation("/faq/");
         <a id="games"></a>
         <h3>Where can I download games?</h3>
         <p>While we cannot officially recommend any single site for downloading Game Boy and Game Boy Color ROMs, we suggest using <a href="https://google.com" target="_blank">Google</a> to search for ROMs by title<sup> <a href="#2">2</a></sup>.</p>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>
         <hr />
+
         <a id="add"></a>
         <h3>How do I add games to Game Play Color?</h3>
         <p>You can add games to Game Play Color by one of two mechanisms:</p>
@@ -60,12 +62,16 @@ navigation("/faq/");
             <iframe width="560" height="315" src="https://www.youtube.com/embed/Eoaq6HVvxY0" frameborder="0" allowfullscreen></iframe>
           </li>
         </ol>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>
         <hr />
+
         <a id="color"></a>
         <h3>How do I change the console color?</h3>
         <p><img src ="/images/ss_colors.png" /></p>
         <p>Like the original Game Boy Color, Game Play Color is available in a range of colors: simply shake your iPhone, iPod or iPad.</p>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>        
         <hr />
+
         <a id="signout"></a>
         <h3>How do I sign out of Google Drive?</h3>
         <p>It's not currently possible to sign out of Google Drive in-app (<a href="https://github.com/jbmorley/gameplay/issues/20">issue #20</a>).</p>
@@ -79,15 +85,21 @@ navigation("/faq/");
           <li>Click <strong>Disconnect</strong> to confirm.<br /><img src="/images/faq_drive_disconnect_app.png" width="546px" height="247px" /></li>
         </ol>
         <p>Next time you launch Game Play, you will be presented with the sign in screen again.</p>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>
         <hr />
+
         <a id="devices"></a>
         <h3>Are there any plans to add Game Boy Advance or Nintendo DS support?</a></h3>
         <p>We're received a lot of requests for Game Boy Advance, Nintendo DS and even some for Nintendo 64 support in Game Play Color.</p>
         <p>We've got our hands full at the moment with the current feature set of Game Play Color but we'll certainly consider additional device support for future products and releases. Obviously we're limited to what's performs well running in JavaScript on a phone so, for example, Nintendo 64 support looks highly unlikely, while Game Boy Advance support might seem more realistic.</p>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>
         <hr />
+
         <a id="multiplayer"></a>
         <h3>Are there any plans to add multiplayer and link cable support?</h3>
         <p>We'd absolutely love to add this feature but we're fairly limited by the environment provided to web apps today (there's no support for peer-to-peer networking). When the appropriate technologies become available, we'll be sure to look again.</p>
+        <p class="faq-top"><a href="#">&#8613; Back to the top</a></p>
+        
       </div>
     </div>
 
