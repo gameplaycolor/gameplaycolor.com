41882c9aa6249d34e217ce3c023086a7a8b93b94
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-04-29 20:33:29 +0200

    Removing excessive use of em and adding anchors for the legal boilerplate.

--- gameplaycolor-microsite/boilerplate.php
+++ gameplaycolor-microsite/boilerplate.php
@@ -2,9 +2,9 @@
 <div class="row-fluid">
   <div spann="span12">
     <ol>
-      <li>Game Boy and Game Boy Color are trademarks of Nintendo Co., Ltd.. All rights reserved.</li>
-      <li>Downloading copied ROMs is illegal: only use images you have created from ROMs you own yourself.</li>
-      <li>InSeven Limited is an independent software company and is in no way affiliated with Nintendo Co., Ltd..</li>
+      <li><a id="1"></a>Game Boy and Game Boy Color are trademarks of Nintendo Co., Ltd.. All rights reserved.</li>
+      <li><a id="2"></a>Downloading copied ROMs is illegal: only use images you have created from ROMs you own yourself.</li>
+      <li><a id="3"></a>InSeven Limited is an independent software company and is in no way affiliated with Nintendo Co., Ltd..</li>
     </ol>
   </div>
 </div>
--- gameplaycolor-microsite/faq.php
+++ gameplaycolor-microsite/faq.php
@@ -26,13 +26,13 @@ navigation("/faq/");
           <li><a href="#games">Where can I download games?</a></li>
           <li><a href="#color">How do I change the console color?</a></li>
           <li><a href="#signout">How do I sign out of Google Drive?</a></li>
-          <li><a href="#devices">Are there any plans to add <em>Game Boy Advance</em> or <em>Nintendo DS</em> support?</a></li>
-          <li><a href="#multiplayer">Are there any plans to add <em>multiplayer</em> and <em>link cable</em> support?</a></li>
+          <li><a href="#devices">Are there any plans to add Game Boy Advance or Nintendo DS support?</a></li>
+          <li><a href="#multiplayer">Are there any plans to add multiplayer and link cable support?</a></li>
         </ol>
         <hr />
         <a id="games"></a>
         <h3>Where can I download games?</h3>
-        <p>While we cannot officially recommend any single site for downloading <em>Game Boy</em> and <em>Game Boy Color</em> ROMs, we suggest using <em>Google</em> to search for ROMs by title<sup> 2</sup>.</p>
+        <p>While we cannot officially recommend any single site for downloading Game Boy and Game Boy Color ROMs, we suggest using <a href="https://google.com" target="_blank">Google</a> to search for ROMs by title<sup> <a href="#2">2</a></sup>.</p>
         <hr />
         <a id="color"></a>
         <h3>How do I change the console color?</h3>
@@ -46,20 +46,20 @@ navigation("/faq/");
         <ol>
           <li>Navigate to <a href="https://drive.google.com" target="_blank">https://drive.google.com</a> in your web browser.</li>
           <li>Sign in using the account you wish to disconnect from Game Play.</li>
-          <li>Click the <em>Gear</em> icon in the top right corner, and then click <em>Settings</em>.<br /><img src="/images/faq_drive_settings.png" width="201px" height="261px" /></li>
-          <li>Select <em>Managing Apps</em>.<br /><img src="/images/faq_drive_settings_apps.png" width="782px" height="426px" /></li>
-          <li>Identify <em>Game Play</em> in the list of apps, click the <em>Options</em> button, and then click <em>Disconnect from Drive</em>.<br /><img src="/images/faq_drive_settings_apps_disconnect.png" width="782px" height="426px" /><br /><em>If you can't find Game Play listed, it's possible you used a signed in with a different Google Drive account.</em></li>
-          <li>Click <em>Disconnect</em> to confirm.<br /><img src="/images/faq_drive_disconnect_app.png" width="546px" height="247px" /></li>
+          <li>Click the <strong>Gear</strong> icon in the top right corner, and then click <strong>Settings</strong>.<br /><img src="/images/faq_drive_settings.png" width="201px" height="261px" /></li>
+          <li>Select <strong>Managing Apps</strong>.<br /><img src="/images/faq_drive_settings_apps.png" width="782px" height="426px" /></li>
+          <li>Identify <strong>Game Play</strong> in the list of apps, click the <strong>Options</strong> button, and then click <strong>Disconnect from Drive</strong>.<br /><img src="/images/faq_drive_settings_apps_disconnect.png" width="782px" height="426px" /><br /><em>If you can't find Game Play listed, it's possible you used a signed in with a different Google Drive account.</em></li>
+          <li>Click <strong>Disconnect</strong> to confirm.<br /><img src="/images/faq_drive_disconnect_app.png" width="546px" height="247px" /></li>
         </ol>
         <p>Next time you launch Game Play, you will be presented with the sign in screen again.</p>
         <hr />
         <a id="devices"></a>
-        <h3>Are there any plans to add <em>Game Boy Advance</em> or <em>Nintendo DS</em> support?</a></h3>
-        <p>We're received a lot of requests for <em>Game Boy Advance</em>, <em>Nintendo DS</em> and even some for <em>Nintendo 64</em> support in Game Play Color.</p>
+        <h3>Are there any plans to add Game Boy Advance or Nintendo DS support?</a></h3>
+        <p>We're received a lot of requests for Game Boy Advance, Nintendo DS and even some for Nintendo 64 support in Game Play Color.</p>
         <p>We've got our hands full at the moment with the current feature set of Game Play Color but we'll certainly consider additional device support for future products and releases. Obviously we're limited to what's performs well running in JavaScript on a phone so, for example, Nintendo 64 support looks highly unlikely, while Game Boy Advance support might seem more realistic.</p>
         <hr />
         <a id="multiplayer"></a>
-        <h3>Are there any plans to add <em>multiplayer</em> and <em>link cable</em> support?</h3>
+        <h3>Are there any plans to add multiplayer and link cable support?</h3>
         <p>We'd absolutely love to add this feature but we're fairly limited by the environment provided to web apps today (there's no support for peer-to-peer networking). When the appropriate technologies become available, we'll be sure to look again.</p>
       </div>
     </div>
