888d79a69fca10626db550fa0685aaddb5060f9c
Jason Barrie Morley <jason.morley@inseven.co.uk>
2016-04-05 14:16:35 +0200

    Removing common PHP code.

--- common/inseven.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once(__DIR__ . '/header.php');
-  require_once(__DIR__ . '/navigation.php');
-  require_once(__DIR__ . '/details.php');
-  require_once(__DIR__ . '/footer.php');
-?>
