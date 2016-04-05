6657aed4f2122807698a5330c0b9f058e559f26e
Jason Barrie Morley <jason.morley@inseven.co.uk>
2016-04-05 14:10:57 +0200

    Migrating a few more pages.

--- gameplaycolor-microsite/common.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once(__DIR__ . '/../common/header.php');
-  require_once(__DIR__ . '/navigation.php');
-  require_once(__DIR__ . '/../common/details.php');
-  require_once(__DIR__ . '/../common/footer.php');
-?>
