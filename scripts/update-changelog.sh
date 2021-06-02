5026f835c66a57ef16dae76d002cce454eda5b05
Jason Barrie Morley <jbmorley@mac.com>
2021-06-01 17:46:45 -0700

    ci: Add a GitHub Actions workflow to update the auto-generated changelog

---
+++ scripts/update-changelog.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
+GAMEPLAYCOLOR_DIRECTORY="${ROOT_DIRECTORY}/gameplaycolor"
+CHANGES_DIRECTORY="${GAMEPLAYCOLOR_DIRECTORY}/scripts/changes"
+
+CHANGES_PATH="${CHANGES_DIRECTORY}/changes"
+CHANGES_PIPFILE_PATH="${CHANGES_DIRECTORY}/Pipfile"
+CHANGELOG_TEMPLATE_PATH="${ROOT_DIRECTORY}/changelog-template.markdown"
+GAMEPLAYCOLOR_HISTORY_PATH="${GAMEPLAYCOLOR_DIRECTORY}/history.yaml"
+CHANGELOG_PATH="${ROOT_DIRECTORY}/docs/changelog/index.markdown"
+
+cd "$ROOT_DIRECTORY"
+
+if [ ! -d "$GAMEPLAYCOLOR_DIRECTORY" ] ; then
+    git clone https://github.com/gameplaycolor/gameplaycolor.git
+fi
+
+cd "$GAMEPLAYCOLOR_DIRECTORY"
+git fetch origin --prune --prune-tags
+git checkout origin/master
+git submodule update --init --recursive
+
+PIPENV_PIPFILE="$CHANGES_PIPFILE_PATH" pipenv install
+"$CHANGES_PATH" notes --all --history "$GAMEPLAYCOLOR_HISTORY_PATH" --template "$CHANGELOG_TEMPLATE_PATH" > "$CHANGELOG_PATH"
